9adedfde36fbc891aed745a9e8b819ca00a12e12
Bob <bob@standardinput.co>
2023-12-26 15:37:44 -0500

    working but too slow

--- 2023/2023-17.py
+++ 2023/2023-17.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from collections import defaultdict
 from collections.abc import Generator
 from dataclasses import dataclass, field
 from typing import (
@@ -48,7 +49,7 @@ CONTROL_1 = """\
 STRAIGHTS = 3
 
 with open("2023-17.input") as f:
-    input = [line.strip() for line in f.readlines()]
+    input_file = [line.strip() for line in f.readlines()]
 
 Pos = NamedTuple("Pos", [("i", int), ("j", int)])
 Pos.__repr__ = lambda x: f"({x.i}, {x.j})"
@@ -71,13 +72,13 @@ class D(str, ReprEnum):
 
     @classmethod
     def detect(cls, prev: Pos, curr: Pos) -> str:
-        if curr.i - prev.i == 1:
+        if curr.i - prev.i > 0:
             return cls.RIGHT
-        if curr.i - prev.i == -1:
+        if curr.i - prev.i < 0:
             return cls.LEFT
-        if curr.j - prev.j == 1:
+        if curr.j - prev.j > 0:
             return cls.DOWN
-        if curr.j - prev.j == -1:
+        if curr.j - prev.j < 0:
             return cls.UP
         return cls.INVALID
 
@@ -146,6 +147,17 @@ def all_straight(coords: list[Pos]) -> bool:
 assert all_straight([Pos(1, 4), Pos(1, 3), Pos(1, 2), Pos(1, 1)])
 
 
+def lerp(a: Pos, b: Pos) -> Iterator[Pos]:
+    if a.i == b.i:
+        step = 1 if a.j < b.j else -1
+        for y in range(a.j, b.j, step):
+            yield Pos(a.i, y)
+    if a.j == b.j:
+        step = 1 if a.i < b.i else -1
+        for x in range(a.i, b.i, step):
+            yield Pos(x, a.j)
+
+
 def visualize_path(input, path: list[Pos]):
     print(f"{Fore.blue}{'-'*len(input[0])}{Style.reset}")
     pathviz: list[list[str]] = [
@@ -155,6 +167,9 @@ def visualize_path(input, path: list[Pos]):
     for n in path[1:]:
         dir = D.detect(p, n)
         pathviz[n.j][n.i] = dir
+        interpolated = list(lerp(p, n))[1:]
+        for inter in interpolated:
+            pathviz[inter.j][inter.i] = dir
         p = n
     for row in pathviz:
         print("".join(row))
@@ -213,46 +228,23 @@ def eval_path(input: Input, path: list[Pos]) -> int:
     return sum(input[c.j][c.i] for c in path)
 
 
-NodeKey = Tuple[Optional[str], Pos, int]
+NodeKey = Tuple[Pos, D]
 
 
 @dataclass
-class Node:
-    coord: Pos
-    cumulative_heat: int
-    prev: Optional[Self] = None
-    times_same_dir: int = 0
-    visited: bool = False
-    in_dir: str = D.INVALID
-    out: set[Self] = field(default_factory=set)
-    v: int = 0
+class QItem:
+    pos: Pos
+    dir: D
+    total_heat: int
+    v: int
 
-    def __repr__(self) -> str:
-        return (
-            f"{self.in_dir}{self.coord}"
-            f" out:{[x.coord for x in self.out]}"
-            f" heat:{self.cumulative_heat} same:{self.times_same_dir}"
-            # f" visited:{'Y' if self.visited else 'N'}"
-        )
-
-    def __hash__(self):
-        return hash(self.key())
-
-    def __lt__(self, other):
-        return self.cumulative_heat < other.cumulative_heat
-
-    def key(self) -> NodeKey:
-        return (self.in_dir, self.coord, self.times_same_dir)
-
-    def dir_from(self, coord: Optional[Pos]) -> str:
-        if not coord:
-            return D.INVALID
-        return D.detect(coord, self.coord)
+    def key(self):
+        return (self.pos, self.dir)
 
 
 class Q:
     q_set: set[NodeKey]
-    q: list[Node]
+    q: list[QItem]
     versions: dict[NodeKey, int]
 
     def __init__(self):
@@ -269,18 +261,18 @@ class Q:
     def __len__(self) -> int:
         return len(self.q)
 
-    def expected_version(self, node: Node) -> int:
+    def expected_version(self, node: QItem) -> int:
         return self.versions.get(node.key(), 0)
 
-    def has_latest_version(self, node: Node) -> bool:
+    def has_latest_version(self, node: QItem) -> bool:
         return self.expected_version(node) == node.v
 
-    def increment_version(self, node: Node):
+    def increment_version(self, node: QItem):
         next_v = self.expected_version(node) + 1
         node.v = next_v
         self.versions[node.key()] = next_v
 
-    def pop(self) -> Optional[Node]:
+    def pop(self) -> Optional[QItem]:
         # node = heapq.heappop(self.q)
         # self.q_set.remove(node)
         # return node
@@ -297,12 +289,12 @@ class Q:
                 pass
             return node
 
-    def push(self, node: Node):
+    def push(self, node: QItem):
         # self.versions[node.key()] = 0
         self.q_set.add(node.key())
         heapq.heappush(self.q, node)
 
-    def update(self, node: Node):
+    def update(self, node: QItem):
         self.increment_version(node)
         self.push(node)
 
@@ -310,141 +302,8 @@ class Q:
         heapq.heapify(self.q)
 
 
-def retrace(
-    prev_nodes: dict[NodeKey, Node], node: Node, limit=1_000
-) -> Generator[Pos, Any, Any]:
-    i = 1
-    yield node.coord
-    while node.prev and i <= limit:
-        yield node.prev.coord
-        node = prev_nodes[node.key()]
-        i += 1
-
-
-_done = set()
-
-
-def populate_graph(
-    rows: int, cols: int, nodes: dict[NodeKey, Node], explored: PSet[Node], curr: Node
-):
-    # if curr.key() in _cache_pop_graph:
-    #     return _cache_pop_graph[curr.key()]
-    if curr.key() in _done:
-        return
-    next_coords = [
-        right(curr.coord),
-        down(curr.coord),
-        left(curr.coord),
-        up(curr.coord),
-    ]
-    next_coords = [x for x in next_coords if in_bounds(rows, cols, x)]
-    neighbors = []
-    for next in next_coords:
-        next_dir = D.detect(curr.coord, next)
-        same = 0
-        if next_dir == curr.in_dir:
-            same = curr.times_same_dir + 1
-        if same > STRAIGHTS:
-            continue
-        next_node = Node(next, BIG_NUM, in_dir=next_dir, times_same_dir=same)
-        if next_node.key() in nodes:
-            next_node = nodes[next_node.key()]
-        neighbors.append(next_node)
-    neighbors = [x for x in neighbors if x not in explored]
-    # neighbors = [x for x in neighbors if x not in curr.out]
-    # out = []
-    for next in neighbors:
-        # print(next)
-        curr.out.add(next)
-        nodes[next.key()] = next
-        populate_graph(rows, cols, nodes, explored.add(curr), next)
-    # _cache_pop_graph[curr.key()] = (curr, out)
-    _done.add(curr.key())
-
-
 def blue_line(length: int):
     print(f"{Fore.blue}{'━'*length}{Style.reset}")
-
-
-# def part_1_3(input):
-#     rows = len(input)
-#     cols = len(input[0])
-#     input = tuple(tuple(int(x) for x in line) for line in input)
-#     blue_line(cols)
-#     print_input(input)
-#     blue_line(cols)
-#     q = Q()
-#     start = Node(Coord(0, 0), cumulative_heat=0)
-#     nodes: dict[NodeKey, Node] = {
-#         start.key(): start,
-#     }
-#     pop_visited: set[NodeKey] = set()
-#     populate_graph(rows, cols, nodes, pyrsistent.s(), start)
-#     nodes_to_q = set()
-#
-#     def q_nodes(n: Node):
-#         nodes_to_q.add(n)
-#         for o in n.out:
-#             if o not in nodes_to_q:
-#                 q_nodes(o)
-#
-#     q_nodes(start)
-#     for node in nodes_to_q:
-#         q.push(node)
-#     last_node = None
-#     prev_nodes: dict[NodeKey, Node] = {}
-#     print(f"POPULATED {len(pop_visited)}")
-#     # for n in pop_visited:
-#     #     if n[2] == 3:
-#     #         print(n)
-#
-#     pp(q.q[:3])
-#     checked = 0
-#     while q:
-#         node = q.pop()
-#         checked += 1
-#         # if checked > 20:
-#         #     last_node = node
-#         #     break
-#         # print(len(q), q.q[:3])
-#         # print(f"{node=}")
-#         if node is None:
-#             break
-#         curr = node.coord
-#         next_coords = [up(curr), right(curr), down(curr), left(curr)]
-#         neighbors = node.out
-#         # neighbors = [x for x in neighbors if x.key() in q]
-#         neighbors = [x for x in neighbors if not x.visited]
-#         for nbr in neighbors:
-#             # print(node, "->", nbr)
-#             edge_heat = input[nbr.coord.j][nbr.coord.i]
-#             tmp_heat = node.cumulative_heat + edge_heat
-#             # print(f"{{{node=}}}->{{{nbr=}}} {edge_heat=}")
-#             if tmp_heat < nbr.cumulative_heat:
-#                 # print("lower")
-#                 # nbr.times_same_dir = times_same_dir
-#                 nbr.cumulative_heat = tmp_heat
-#                 # TODO: use update to push another node
-#                 # q.update(nbr)
-#                 # q.increment_version(nbr)
-#                 nbr.prev = node
-#                 prev_nodes[nbr.key()] = node
-#                 if nbr.coord == Coord(cols - 1, rows - 1):
-#                     last_node = nbr
-#         # TODO: eliminate
-#         q.reheap()
-#         node.visited = True
-#     print("checked", checked)
-#     if last_node is None:
-#         raise Exception("FUCK, checked", checked)
-#     print(f"{last_node.cumulative_heat=}")
-#     path = list(retrace(prev_nodes, last_node))
-#     path.reverse()
-#     print(path)
-#     # print(f"{len(path)}")
-#     visualize_path(input, path)
-#     total_heat_loss = sum(input[n.j][n.i] for n in path)
-#     return total_heat_loss
 
 
 class Field:
@@ -499,98 +358,66 @@ def part_1_4(input):
                 right_heat += field.get(p)
                 yield p, D.RIGHT, right_heat
 
-    discovered = set()
-
-    @dataclass
-    class Node:
-        pos: Pos
-        in_dir: D
-        heat: int
-
-        def __hash__(self):
-            return hash((self.pos, self.in_dir))
-
-    # def dfs():
-    #     start = Pos(0, 0)
-    #     end = Pos(field.cols - 1, field.rows - 1)
-    #     adj_list = DefaultDict(set)
-    #     stack = [
-    #         (Node(start, D.RIGHT, field.get(start)), {start}, field.get(start)),
-    #         (Node(start, D.DOWN, field.get(start)), {start}, field.get(start)),
-    #     ]
-    #     lowest_terminal = BIG_NUM
-    #     while stack:
-    #         node, path, path_heat = stack.pop()
-    #         if path_heat > lowest_terminal:
-    #             continue
-    #         if node.pos == end:
-    #             summed = sum(field.get(p) for p in path)
-    #             if path_heat < lowest_terminal:
-    #                 lowest_terminal = path_heat
-    #             print("terminal", summed, path_heat, "lowest:", lowest_terminal)
-    #             visualize_path(field.input, list(path))
-    #             continue
-    #         if node not in discovered:
-    #             discovered.add(node)
-    #             for p, d, h in next_nodes(node.in_dir, node.pos):
-    #                 if p in path:
-    #                     continue
-    #                 n = Node(p, d, h)
-    #                 adj_list[node].add(n)
-    #                 next_path = path.copy()
-    #                 path.add(p)
-    #                 stack.append((n, next_path, path_heat + h))
-    #             discovered.remove(node)
-    #
-    # expanse = dfs()
-    def expand(field):
-        # TODO: iterate through each cell and throw its possible neighbors
-        # into the adjacency list.
+    def expand(field) -> dict[tuple[Pos, D], set[tuple[Pos, D, int]]]:
         adj_list = DefaultDict(set)
+        for j in range(field.rows):
+            for i in range(field.cols):
+                pos = Pos(i, j)
+                for dir in (D.DOWN, D.RIGHT, D.LEFT, D.UP):
+                    for n in next_nodes(dir, pos):
+                        adj_list[pos, dir].add(n)
+        return adj_list
 
-    # def dijkstra():
-    #     start = Pos(0, 0)
-    #     end = Pos(field.cols - 1, field.rows - 1)
-    #     expanded = set()
-    #     heat = [[BIG_NUM for c in row] for row in field.input]
-    #     heat[start.j][start.i] = 0
-    #     prev = {}
-    #     frontier = []
-    #     frontier.append((start, D.RIGHT, field.get(start), [start], field.get(start)))
-    #     frontier.append((start, D.DOWN, field.get(start), [start], field.get(start)))
-    #     while frontier:
-    #         frontier.sort(key=lambda x: -heat[x[0].j][x[0].i])
-    #         pos, in_dir, heat_, path, total_heat = frontier.pop()
-    #         if pos == end:
-    #             print("end")
-    #             return
-    #         expanded.add((pos, in_dir))
-    #         for n, d, h in next_nodes(in_dir, pos):
-    #             if n not in expanded and n not in frontier:
-    #                 frontier.append(n)
-    #             elif n in frontier with higher cost:
-    #                 # frontier.
-    #                 # replace existing node with n
-    #             else:
-    #                 panic!
-    #             alt = heat[pos.j][pos.i] + field.get(n)
-    #             if alt < heat[n.j][n.i]:
-    #                 heat[n.j][n.i] = alt
-    #                 prev[n] = pos
-    #     return heat, prev
-    #
-    # heat, prev = dijkstra()
+    def dijkstra():
+        start = Pos(0, 0)
+        adj_list = expand(field)
+        q = list(adj_list.keys())
+        # q = Q()
+        print("POPULATED QUEUE, EXPLORING")
+        heat: DefaultDict[tuple[Pos, D], int] = defaultdict(lambda: BIG_NUM)
+        heat[start, D.RIGHT] = 0
+        heat[start, D.DOWN] = 0
+        prev: dict[tuple[Pos, D], tuple[Pos, D]] = {}
+        while q:
+            q.sort(key=lambda x: -heat[x])
+            print("sort")
+            pos, dir = q.pop()
+            for npos, ndir, nheat in adj_list[pos, dir]:
+                if (npos, ndir) not in q:
+                    continue
+                alt = heat[pos, dir] + nheat
+                if alt < heat[npos, ndir]:
+                    heat[npos, ndir] = alt
+                    prev[npos, ndir] = pos, dir
+        return heat, prev
 
-    def retrace(prev: dict[Pos, Pos], end: Pos):
+    heat, prev = dijkstra()
+
+    def retrace(prev: dict[tuple[Pos, D], tuple[Pos, D]], end: Pos, end_dir: D):
         yield end
-        n = prev[end]
-        while n != Pos(-1, -1):
-            yield n
-            n = prev[n]
+        n = prev.get((end, end_dir))
+        while n is not None:
+            yield n[0]
+            n = prev.get(n)
 
     # pp(heat)
-    # print(list(retrace(prev, Pos(field.cols - 1, field.rows - 1))))
-
+    # pp(prev)
+    start = Pos(0, 0)
+    end = Pos(field.cols - 1, field.rows - 1)
+    path = list(retrace(prev, end, D.RIGHT))
+    path.reverse()
+    print(path)
+    visualize_path(input, path)
+    total_heat = 0
+    for p, n in pairwise(path):
+        interpolated = list(lerp(p, n))
+        for inter in interpolated:
+            print(inter, "=", input[inter.j][inter.i])
+            total_heat += input[inter.j][inter.i]
+    total_heat -= input[start.j][start.i]
+    total_heat += input[end.j][end.i]
+    # total_heat = sum(input[p.j][p.i] for p in path)
+    print(f"{total_heat=}")
     return 0
 
 
