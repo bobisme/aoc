b43d5843a3eb3bba127ac988c0ed9d301eafad75
Bob <bob@standardinput.co>
2024-12-21 17:12:17 -0500

    2024-17 p2 works for example, not input

--- 2024/17/2024-17.py
+++ 2024/17/2024-17.py
@@ -156,13 +156,13 @@ class Computer:
         self.c = math.trunc(self.a / (2 ** self.combo(operand)))
         self.pointer += 2
 
-    def execute(self) -> str:
+    def execute(self) -> list[int]:
         while True:
             try:
                 opcode, operand = self.read_ops(self.pointer)
                 self.handle(opcode, operand)
             except Halt:
-                return ",".join(str(x) for x in self.output)
+                return self.output
 
 
 def part_1(input: Iterable[str]):
@@ -178,14 +178,14 @@ def part_1(input: Iterable[str]):
     def test_2():
         comp = Computer({"A": 10}, [5, 0, 5, 1, 5, 4])
         out = comp.execute()
-        assert out == "0,1,2", f"{repr(comp)} {out}"
+        assert out == [0, 1, 2], f"{repr(comp)} {out}"
         print("test 2 passed")
 
     def test_3():
         comp = Computer({"A": 2024}, [0, 1, 5, 4, 3, 0])
         out = comp.execute()
         assert comp.a == 0, f"{repr(comp)} {out}"
-        assert out == "4,2,5,6,7,7,7,7,3,1,0", f"{repr(comp)} {out}"
+        assert out == [4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0], f"{repr(comp)} {out}"
         print("test 3 passed")
 
     def test_4():
@@ -200,41 +200,112 @@ def part_1(input: Iterable[str]):
         assert comp.b == 44354, f"{repr(comp)} {out}"
         print("test 5 passed")
 
-    test_1()
-    test_2()
-    test_3()
-    test_4()
-    test_5()
+    # test_1()
+    # test_2()
+    # test_3()
+    # test_4()
+    # test_5()
 
     registers, program = parse(input)
     computer = Computer(registers, program)
-    print(computer.execute())
+    print(",".join(str(x) for x in computer.execute()))
 
 
-def part_2(input):
-    def test_6():
-        registers, program = parse(CONTROL_2)
-        comp = Computer(registers, program)
-        comp.a = 117440
-        out = comp.execute()
-        assert out == "0,3,5,4,3,0", f"{repr(comp)} {out}"
-        print("test 6 passed")
+def search(
+    registers: Registers, program: list[int], idx: int, lo=0, hi=100_000_000_000_000
+) -> tuple[int, int, int]:
+    target = program[idx]
+    while lo < hi:
+        mid_a = (lo + hi) // 2
+        computer = Computer(registers, program)
+        computer.a = mid_a
+        mid_output = computer.execute()
+        try:
+            mid_val = mid_output[idx]
+        except IndexError:
+            mid_val = -1
+        if len(mid_output) > len(program):
+            mid_val = sys.maxsize
+        if mid_val < target:
+            lo = mid_a + 1
+        elif mid_val > target:
+            hi = mid_a
+        else:
+            return mid_a, lo, hi
+    return -1, -1, -1
 
-    test_6()
 
-    registers, program = parse(input)
-    input = ",".join(str(x) for x in program)
-    for a in range(50_000_000_000_000, sys.maxsize):
+def program_to_octal(program: list[int]) -> int:
+    return int("".join(str(x) for x in reversed(program)), 8)
+
+
+def search_oct(registers: Registers, program: list[int], lo=0, hi=sys.maxsize) -> int:
+    oct_prg = program_to_octal(program)
+    mid_a = -1
+    while lo < hi:
+        mid_a = (lo + hi) // 2
+        computer = Computer(registers, program)
+        computer.a = mid_a
+        output = computer.execute()
+        if len(output) < len(program):
+            mid_val = -1
+        elif len(output) > len(program):
+            mid_val = sys.maxsize
+        else:
+            mid_val = program_to_octal(output)
+        print(mid_a, mid_val)
+        if mid_val < oct_prg:
+            lo = mid_a + 1
+        elif mid_val > oct_prg:
+            hi = mid_a
+        else:
+            return mid_a
+    raise Exception("search failed")
+
+
+def find_lowest(registers: Registers, program: list[int], hi_a: int) -> int:
+    out_a = hi_a
+    oct_prg = program_to_octal(program)
+    for a in range(hi_a, -1, -1):
         computer = Computer(registers, program)
         computer.a = a
         output = computer.execute()
-        if a % 1000 == 0:
-            print("checked", a, "|", input, "|", output)
-        if input == output:
-            print(a, input, output)
+        oct_out = program_to_octal(output)
+        print("looking for lowest", hi_a, oct_out, oct_prg)
+        if oct_out < oct_prg:
             break
+        out_a = a
+    return out_a
+
+
+def part_2(input):
+    # def test_6():
+    #     registers, program = parse(CONTROL_2)
+    #     comp = Computer(registers, program)
+    #     comp.a = 117440
+    #     out = comp.execute()
+    #     assert out == [0, 3, 5, 4, 3, 0], f"{repr(comp)} {out}"
+    #     print("test 6 passed")
+    #
+    # test_6()
+    #
+    registers, program = parse(input)
+    # mid_a = search_oct(registers, program)
+    # computer = Computer(registers, program)
+    # computer.a = mid_a
+    # output = computer.execute()
+    # print(mid_a, program, output)
+    #
+    # low_a = find_lowest(registers, program, mid_a)
+    # print("low a", low_a)
+    for sig in range(0, 8):
+        a = sig << (3 * (len(program) - 2))
+        computer = Computer(registers, program)
+        computer.a = a
+        output = computer.execute()
+        print(output)
 
 
 if __name__ == "__main__":
     part_1(input_file)
-    part_2(input_file)
+    part_2(CONTROL_1)
