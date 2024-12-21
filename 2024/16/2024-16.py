4a42cced50f33d37a2bdd13423e3a88923f294fb
Bob <bob@standardinput.co>
2024-12-21 14:00:09 -0500

    2024-16

--- 2024/16/2024-16.py
+++ 2024/16/2024-16.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
+from collections import deque
 from collections.abc import Iterable
+import heapq
 from dataclasses import dataclass
+from pprint import pp
 import sys
-from typing import Deque, Literal
+from typing import DefaultDict, Deque, Literal
+
+DIM = "\033[2m"
+BRIGHT = "\033[1m"
+RESET = "\033[0m"
 
 CONTROL_1 = """\
 ###############
@@ -121,6 +128,9 @@ def get_next(pos: Pos, dir: Dir) -> Pos:
             return Pos(pos.i, pos.j - 1)
 
 
+State = tuple[Pos, Dir]
+
+
 class Node:
     pos: Pos
     dir: Dir
@@ -131,55 +141,107 @@ class Node:
         self.dir = dir
         self.cost = cost
 
+    @property
+    def state(self) -> State:
+        return (self.pos, self.dir)
+
     def __repr__(self) -> str:
         return f"{self.pos} {self.dir} {self.cost}"
 
     def __hash__(self) -> int:
         return hash((self.pos, self.dir))
 
+    def __lt__(self, other: "Node") -> bool:
+        return self.cost < other.cost
 
-def search(grid: Grid, start: Node) -> int:
-    q: Deque[Node] = Deque()
-    q.append(start)
-    lowest = sys.maxsize
-    visited: set[tuple[Pos, Dir]] = set()
 
-    while q:
-        n = q.popleft()
-        # print(n)
-        visited.add((n.pos, n.dir))
-        if grid.grid[n.pos.i][n.pos.j] == "E":
-            if n.cost < lowest:
-                lowest = n.cost
-                # print("new lower cost", lowest)
+PrevMap = dict[State, set[State]]
+
+
+def dijkstra(grid: Grid, start: Node) -> tuple[int, PrevMap]:
+    distances = {(start.pos, start.dir): start.cost}  # (pos, dir) -> cost
+    pq = [(start.cost, start)]
+    prev_map: DefaultDict[State, set[State]] = DefaultDict(set)
+    min_end_cost = sys.maxsize
+
+    while pq:
+        curr_cost, curr_node = heapq.heappop(pq)
+
+        if grid.grid[curr_node.pos.i][curr_node.pos.j] == "E":
+            if curr_cost < min_end_cost:
+                min_end_cost = curr_cost
             continue
-        next_nodes = [
-            Node(n.pos, turn_left(n.dir), n.cost + 1000),
-            Node(n.pos, turn_right(n.dir), n.cost + 1000),
+
+        if curr_cost > distances[(curr_node.pos, curr_node.dir)]:
+            continue
+
+        # Generate next possible moves
+        next_moves: list[tuple[Dir, int]] = [
+            (turn_left(curr_node.dir), 1001),  # Left turn cost
+            (turn_right(curr_node.dir), 1001),  # Right turn cost
+            (curr_node.dir, 1),  # Forward cost
         ]
-        next_pos = get_next(n.pos, n.dir)
-        if grid.grid[next_pos.i][next_pos.j] != "#":
-            next_nodes.append(Node(next_pos, n.dir, n.cost + 1))
-        for nn in next_nodes:
-            if (nn.pos, nn.dir) in visited:
-                continue
-            if nn.cost > lowest:
-                continue
-            q.append(nn)
-    return lowest
+
+        for next_dir, move_cost in next_moves:
+            next_pos = get_next(curr_node.pos, next_dir)
+            if grid.grid[next_pos.i][next_pos.j] != "#":
+                next_cost = curr_cost + move_cost
+                next_state = (next_pos, next_dir)
+
+                if next_state not in distances or next_cost <= distances[next_state]:
+                    next_node = Node(next_pos, next_dir, next_cost)
+                    if next_state not in distances or next_cost < distances[next_state]:
+                        distances[next_state] = next_cost
+                        prev_map[next_state] = set()
+
+                    distances[next_state] = next_cost
+                    prev_map[next_state].add(curr_node.state)
+                    heapq.heappush(pq, (next_cost, next_node))
+
+    return min_end_cost, prev_map
 
 
 def part_1(input):
     grid = Grid(input)
-    lowest = search(grid, Node(Pos(grid.width - 2, 1), ">", 0))
+    lowest, _ = dijkstra(grid, Node(Pos(grid.width - 2, 1), ">", 0))
     print(lowest)
 
 
+def map_paths(end_pos: Pos, prev_map: PrevMap) -> set[Pos]:
+    positions = {end_pos}
+    end_state = next(k for (k, v) in prev_map.items() if k[0] == end_pos)
+    state = end_state
+    q = deque([end_state])
+    v = set()
+    while q:
+        state = q.popleft()
+        v.add(state)
+        positions.add(state[0])
+        prev_states = prev_map[state]
+        for prev_state in prev_states:
+            if prev_state in v:
+                continue
+            q.append(prev_state)
+    return positions
+
+
+def print_path_positions(grid: Grid, positions: Iterable[Pos]):
+    g = [[f"{DIM}{x}{RESET}" for x in row] for row in grid.grid]
+    for p in positions:
+        g[p.i][p.j] = f"{BRIGHT}O{RESET}"
+    for row in g:
+        print("".join(row))
+
+
 def part_2(input):
-    for line in input:
-        print(line)
+    grid = Grid(input)
+    lowest, prev_map = dijkstra(grid, Node(Pos(grid.width - 2, 1), ">", 0))
+    end_pos = Pos(1, grid.width - 2)
+    positions = map_paths(end_pos, prev_map)
+    print_path_positions(grid, positions)
+    print(len(positions))
 
 
 if __name__ == "__main__":
     part_1(input_file)
-    # part_2(input_file)
+    part_2(input_file)
