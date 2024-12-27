168c65252454e08efc20a59cf75907b9b55c2701
Bob <bob@standardinput.co>
2024-12-27 12:48:45 -0500

    2024-11: p2

--- 2024/11/2024-11.py
+++ 2024/11/2024-11.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from typing import Deque, Generator
+from typing import DefaultDict, Deque, Generator
 from itertools import chain
 
 
@@ -31,53 +31,41 @@ def blink_slow(stones: Deque[int]):
         i += 1
 
 
-def blinks(stones: Deque[int], times=1):
-    map: dict[int, list[int]] = {}
+def blink_fast(stones: Deque[int], times=1):
+    old_cache: DefaultDict[int, int] = DefaultDict(int)  # stone -> count
+    for stone in stones:
+        old_cache[stone] += 1
 
-    def blink(stone: int) -> list[int]:
-        if stone in map:
-            return map[stone]
-        if stone == 0:
-            map[stone] = [1]
-            return [1]
-        elif len(str(stone)) % 2 == 0:
-            str_stone = str(stone)
-            left = int(str_stone[: len(str_stone) // 2])
-            right = int(str_stone[len(str_stone) // 2 :])
-            map[stone] = [left, right]
-            return [left, right]
-        else:
-            map[stone] = [stone * 2024]
-            return [stone * 2024]
+    for i in range(times):
+        new_cache: DefaultDict[int, int] = DefaultDict(int)
+        for stone, count in old_cache.items():
+            if stone == 0:
+                new_cache[1] += count
+            elif len(str(stone)) % 2 == 0:
+                str_stone = str(stone)
+                left = int(str_stone[: len(str_stone) // 2])
+                right = int(str_stone[len(str_stone) // 2 :])
+                new_cache[left] += count
+                new_cache[right] += count
+            else:
+                new_cache[stone * 2024] += count
+        old_cache = new_cache
 
-    def blink_gen(
-        stones: list[int], count: int = 1
-    ) -> Generator[list[int], None, None]:
-        for _ in range(count):
-            yield list(chain(*(blink(stone) for stone in stones)))
-
-    # for stone in stones:
-    #     sss = [stone]
-    #     for _ in range(times):
-    #         for s in sss:
-    #             if s in map:
+    return sum(old_cache.values())
 
 
 def part_1(input):
     stones = Deque(int(x) for x in input[0].split(" "))
-    for _ in range(25):
-        blink_slow(stones)
-    print(len(stones))
+    out = blink_fast(stones, times=25)
+    print(out)
 
 
 def part_2(input):
     stones = Deque(int(x) for x in input[0].split(" "))
-    for _ in range(75):
-        blink_slow(stones)
-        print(stones)
-    print(len(stones))
+    out = blink_fast(stones, times=75)
+    print(out)
 
 
 if __name__ == "__main__":
-    # part_1(input_file)
-    part_2(CONTROL_1)
+    part_1(input_file)
+    part_2(input_file)
