1e4de7dbdc8917b9afaf111bc6d4435602467166
Bob <bob@standardinput.co>
2025-12-07 02:49:28 -0500

    2025-07: clean up & small optimization

--- 2025/2025-07.py
+++ 2025/2025-07.py
@@ -34,54 +34,43 @@ Pos = NamedTuple("Pos", [("i", int), ("j", int)])
 
 def part_1(input: Input):
     grid = [[c for c in row] for row in input]
-    h, w = len(grid), len(grid[0])
+    h = len(grid)
     s_pos = Pos(0, next(i for (i, x) in enumerate(grid[0]) if x == "S"))
 
-    def shoot(pos: Pos) -> Generator[Pos]:
-        if pos.i < 0 or pos.i >= h or pos.j < 0 or pos.j >= w:
-            return
+    def shoot(pos: Pos) -> int:
         for i in range(pos.i, h):
             if grid[i][pos.j] == "|":
-                return
+                return 0
             if grid[i][pos.j] == "^":
-                yield Pos(i, pos.j)
-                yield from shoot(Pos(i, pos.j - 1))
-                yield from shoot(Pos(i, pos.j + 1))
-                return
+                return 1 + shoot(Pos(i, pos.j - 1)) + shoot(Pos(i, pos.j + 1))
             else:
                 grid[i][pos.j] = "|"
+        return 0
 
-    positions = set(shoot(Pos(s_pos.i + 1, s_pos.j)))
-    return len(positions)
+    return shoot(s_pos)
 
 
 def part_2(input: Input):
-    grid = [[c for c in row] for row in input]
-    h, w = len(grid), len(grid[0])
-    s_pos = Pos(0, next(i for (i, x) in enumerate(grid[0]) if x == "S"))
+    h = len(input)
+    s_pos = Pos(0, next(i for (i, x) in enumerate(input[0]) if x == "S"))
 
-    cache_grid = [[0 for _ in row] for row in grid]
+    cache_map = {}
 
     def shoot(pos: Pos) -> int:
-        if pos.i < 0 or pos.i >= h or pos.j < 0 or pos.j >= w:
-            return 0
-        if (val := cache_grid[pos.i][pos.j]) > 0:
+        if (val := cache_map.get(pos)) is not None:
             return val
-        sub_path = []
         for i in range(pos.i, h):
-            if grid[i][pos.j] == "^":
-                val = +shoot(Pos(i, pos.j - 1)) + shoot(Pos(i, pos.j + 1))
-                for p in sub_path:
-                    cache_grid[p.i][p.j] = val
+            if input[i][pos.j] == "^":
+                val = shoot(Pos(i, pos.j - 1)) + shoot(Pos(i, pos.j + 1))
+                for ii in range(pos.i, i):
+                    cache_map[Pos(ii, pos.j)] = val
                 return val
-            sub_path.append(Pos(i, pos.j))
-        for p in sub_path:
-            cache_grid[p.i][p.j] = 1
+        for ii in range(pos.i, h):
+            cache_map[Pos(ii, pos.j)] = 1
         return 1
 
     shoot(s_pos)
-
-    return cache_grid[s_pos.i][s_pos.j]
+    return cache_map[s_pos]
 
 
 def _test():
