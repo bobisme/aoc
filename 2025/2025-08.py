387cdeee47e3a20c608478152c06bc96013a4e31
Bob <bob@standardinput.co>
2025-12-08 18:03:22 -0500

    2025-08: perf & cleanup: p1=426ms, p2=510ms

--- 2025/2025-08.py
+++ 2025/2025-08.py
@@ -41,9 +41,15 @@ Distances = list[tuple[float, tuple[int, int]]]
 
 class Connections:
     map: DefaultDict[int, set[int]]
+    # full circuits
+    circuit_map: dict[int, set[int]]
+    # map of set id (pointer) to set of box index which point to that set
+    id_map: dict[int, set[int]]
 
-    def __init__(self):
+    def __init__(self, boxes: list[Box]):
         self.map = DefaultDict(set)
+        self.circuit_map = {i: {i} for i in range(len(boxes))}
+        self.id_map = {id(s): {i} for i, s in self.circuit_map.items()}
 
     def __contains__(self, x) -> bool:
         return x in self.map
@@ -51,9 +57,22 @@ class Connections:
     def __getitem__(self, i: int) -> set[int]:
         return self.map[i]
 
+    # TODO: This is broken. Doesn't work for p1, but works for p2.
+    def merge_circuits(self, i: int, j: int):
+        i_circuit = self.circuit_map[i]
+        j_circuit = self.circuit_map[j]
+        i_id, j_id = id(i_circuit), id(j_circuit)
+        if i_id == j_id:
+            return
+        i_circuit |= j_circuit
+        for other_i in self.id_map[j_id]:
+            self.circuit_map[other_i] = i_circuit
+        self.id_map[i_id].add(j)
+
     def connect(self, i: int, j: int):
         self.map[i].add(j)
         self.map[j].add(i)
+        self.merge_circuits(i, j)
 
     def full_circuit(self, box_i: int) -> set[int]:
         circuit = {box_i}
@@ -70,32 +89,33 @@ class Connections:
 
 
 def get_distances(boxes: list[Box]) -> Distances:
-    distances = {}
+    distances = []
     for i in range(len(boxes) - 1):
         for j in range(i + 1, len(boxes)):
-            distances[(i, j)] = math.dist(boxes[i], boxes[j])
-    return list(
-        sorted(
-            ((dist, (i, j)) for ((i, j), dist) in distances.items()), key=lambda x: x[0]
-        )
-    )
+            distances.append((math.dist(boxes[i], boxes[j]), (i, j)))
+    distances.sort(key=lambda x: x[0])
+    return distances
 
 
-def closest_boxes(distances: Distances, connections: Connections) -> tuple[int, int]:
-    for _, (i, j) in distances:
+def closest_boxes(
+    distances: Distances, connections: Connections, offset=0
+) -> tuple[tuple[int, int], int]:
+    for idx in range(offset, len(distances)):
+        _, (i, j) = distances[idx]
         if i in connections and j in connections[i]:
             continue
-        return (i, j)
+        return (i, j), idx
     raise ValueError("no more connections possible")
 
 
 def part_1(input: Input, max_conn_count=1_000):
     boxes = [Box(*map(int, line.split(","))) for line in input]
-    connections = Connections()
+    connections = Connections(boxes)
     distances = get_distances(boxes)
 
+    offset = 0
     for _ in range(max_conn_count):
-        (i, j) = closest_boxes(distances, connections)
+        (i, j), offset = closest_boxes(distances, connections, offset=offset)
         connections.connect(i, j)
 
     checked = set()
@@ -114,17 +134,17 @@ def part_1(input: Input, max_conn_count=1_000):
 
 def part_2(input: Input):
     boxes = [Box(*map(int, line.split(","))) for line in input]
-    connections = Connections()
+    connections = Connections(boxes)
     distances = get_distances(boxes)
 
-    last_connection = (0, 0)
+    connection = (0, 0)
+    offset = 0
     while True:
-        (i, j) = closest_boxes(distances, connections)
-        last_connection = (i, j)
-        connections.connect(i, j)
-        if len(connections.full_circuit(i)) >= len(boxes):
+        connection, offset = closest_boxes(distances, connections, offset=offset)
+        connections.connect(*connection)
+        if len(connections.circuit_map[connection[0]]) >= len(boxes):
             break
-    return boxes[last_connection[0]].x * boxes[last_connection[1]].x
+    return boxes[connection[0]].x * boxes[connection[1]].x
 
 
 def _test():
@@ -142,6 +162,7 @@ def _bench(fn, count=100):
 if __name__ == "__main__":
     _test()
     print("tests: PASS")
+    boxes = [Box(*map(int, line.split(","))) for line in input_file]
     print("-" * 40)
     print("part_1:", part_1(input_file))
     print("part_2:", part_2(input_file))
