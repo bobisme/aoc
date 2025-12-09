36e4eae09da64ff7253d4c6597544a0206566859
Bob <bob@standardinput.co>
2025-12-09 02:36:12 -0500

    2025-09: p2 **

--- 2025/2025-09.py
+++ 2025/2025-09.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from functools import cache
 from typing import LiteralString, NamedTuple
 import timeit
 
@@ -22,28 +23,122 @@ with open("2025-09.input") as f:
     input_file = [line.rstrip("\n") for line in f.readlines()]
 
 Pos = NamedTuple("Pos", [("x", int), ("y", int)])
+Line = NamedTuple("Line", [("a", Pos), ("b", Pos)])
 
 
-def areas(positions: list[Pos]) -> list[int]:
+def area(a: Pos, b: Pos) -> int:
+    return (abs(b.x - a.x) + 1) * (abs(b.y - a.y) + 1)
+
+
+def get_areas(positions: list[Pos]) -> list[tuple[int, tuple[int, int]]]:
     areas = []
     for i in range(len(positions) - 1):
         a = positions[i]
         for j in range(i + 1, len(positions)):
             b = positions[j]
-            areas.append((abs(b.x - a.x) + 1) * (abs(b.y - a.y) + 1))
+            areas.append((area(a, b), (i, j)))
     return areas
 
 
 def part_1(input: Input):
     positions = [Pos(*map(int, line.split(","))) for line in input]
-    areas_ = areas(positions)
-    return max(areas_)
+    areas = get_areas(positions)
+    return max(areas, key=lambda x: x[0])[0]
+
+
+def print_grid(positions: list[Pos]):
+    max_x = max(p.x for p in positions)
+    max_y = max(p.y for p in positions)
+    grid = [["." for _ in range(max_x + 2)] for _ in range(max_y + 2)]
+    for p in positions:
+        grid[p.y][p.x] = "#"
+    for line in grid:
+        print("".join(line))
 
 
 def part_2(input: Input):
     positions = [Pos(*map(int, line.split(","))) for line in input]
-    # for line in input:
-    #     print(line)
+    areas = get_areas(positions)
+    areas.sort(key=lambda x: -x[0])
+    compressed_positions = positions.copy()
+    pos_by_x = sorted(enumerate(positions), key=lambda x: x[1].x)
+    pos_by_y = sorted(enumerate(positions), key=lambda x: x[1].y)
+
+    last_x = pos_by_x[0][1].x
+    offset = 0
+    for i, p in pos_by_x:
+        if p.x == last_x:
+            compressed_positions[i] = Pos(p.x - last_x + offset, p.y)
+            continue
+        last_x = p.x
+        offset += 2
+        compressed_positions[i] = Pos(p.x - last_x + offset, p.y)
+
+    last_y = pos_by_y[0][1].y
+    offset = 0
+    for i, p in pos_by_y:
+        if p.y == last_y:
+            compressed_positions[i] = Pos(
+                compressed_positions[i].x, p.y - last_y + offset
+            )
+            continue
+        last_y = p.y
+        offset += 2
+        compressed_positions[i] = Pos(compressed_positions[i].x, p.y - last_y + offset)
+
+    def lines(positions: list[Pos]):
+        for i in range(len(positions) - 1):
+            a = positions[i]
+            b = positions[i + 1]
+            yield Line(a, b)
+        yield Line(positions[-1], positions[0])
+
+    def intersects(ray: Pos, line: Line) -> bool:
+        # vertical line
+        if line.a.x == line.b.x:
+            line_a, line_b = line.a, line.b
+            if line_a.y > line_b.y:
+                line_a, line_b = line_b, line_a
+            return (line_a.y <= ray.y <= line_b.y) and ray.x <= line.a.x
+        # horizontal
+        line_a, line_b = line.a, line.b
+        if line_a.x > line_b.x:
+            line_a, line_b = line_b, line_a
+        return ray.y == line_a.y and ray.x <= line_b.x
+
+    @cache
+    def point_inside(p: Pos) -> bool:
+        intersections = [
+            line for line in lines(compressed_positions) if intersects(p, line)
+        ]
+        intersect_count = len(intersections)
+        return intersect_count % 2 != 0
+
+    def get_check_point(a: Pos, b: Pos) -> Pos:
+        return Pos(
+            x=a.x + (1 if b.x > a.x else (-1 if b.x < a.x else 0)),
+            y=a.y + (1 if b.y > a.y else (-1 if b.y < a.y else 0)),
+        )
+
+    for idx, (area, (i, j)) in enumerate(areas):
+        a = compressed_positions[i]
+        b = compressed_positions[j]
+        if (idx + 1) % 1000 == 0:
+            print(f"checked {idx+1}/{len(areas)}")
+        check_bounds = (
+            get_check_point(a, b),
+            get_check_point(b, a),
+        )
+        min_x = min(p.x for p in check_bounds)
+        min_y = min(p.y for p in check_bounds)
+        max_x = max(p.x for p in check_bounds)
+        max_y = max(p.y for p in check_bounds)
+        check_points = (
+            Pos(x, y) for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)
+        )
+        if all(point_inside(p) for p in check_points):
+            return area
+
     return 0
 
 
@@ -52,7 +147,7 @@ def _test():
         assert a == b, f"{a} != {b}"
 
     assert_eq(part_1(CONTROL_1), 50)
-    # assert_eq(part_2(CONTROL_1), 0)
+    assert_eq(part_2(CONTROL_1), 24)
 
 
 def _bench(fn, count=100):
@@ -67,4 +162,4 @@ if __name__ == "__main__":
     print("part_2:", part_2(input_file))
     print("-" * 40)
     print("part_1 bench: {:.1f}ms".format(_bench(lambda: part_1(input_file), count=1)))
-    print("part_2 bench: {:.1f}ms".format(_bench(lambda: part_2(input_file), count=1)))
+    # print("part_2 bench: {:.1f}ms".format(_bench(lambda: part_2(input_file), count=1)))
