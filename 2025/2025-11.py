8e1c13f8b4b760136d7c0669c403acf6d00e3c55
Bob <bob@standardinput.co>
2025-12-11 02:24:52 -0500

    2025-11: p2 **

--- 2025/2025-11.py
+++ 2025/2025-11.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 from collections import deque
-from typing import DefaultDict, LiteralString
+from functools import reduce
+from typing import DefaultDict, Iterable, LiteralString
 import timeit
 
 Input = list[str] | list[LiteralString]
@@ -21,6 +22,24 @@ iii: out
 """.splitlines()
 )
 
+CONTROL_2: Input = (
+    """\
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out
+""".splitlines()
+)
+
 with open("2025-11.input") as f:
     input_file = [line.rstrip("\n") for line in f.readlines()]
 
@@ -35,32 +54,83 @@ def parse(input: Input) -> Graph:
     return g
 
 
-def part_1(input: Input):
-    g = parse(input)
+def bfs_count(graph: Graph, src: str, dst: str) -> int:
+    "Slow BFS count."
     q = deque()
-    q.append(("you", set()))
-    visited = set()
+    q.append([src])
     path_count = 0
     while q:
-        node, path = q.popleft()
-        if node == "out":
+        path = q.popleft()
+        node = path[-1]
+        if node == dst:
             path_count += 1
             continue
-        if node in visited:
+        if node == "out":
             continue
-        next_path = path | {node}
-        for next_node in g[node]:
-            if next_node in next_path:
+        for next_node in graph[node]:
+            if next_node in path:
                 continue
-            q.append((next_node, next_path))
-
+            q.append(path + [next_node])
     return path_count
 
 
+def dfs_count(
+    graph: Graph,
+    src: str,
+    dst: str,
+    path: list[str] | None = None,
+    counts: DefaultDict[str, int] | None = None,
+) -> int:
+    "Fast DFS count."
+    if counts is None:
+        counts = DefaultDict(int)
+    if path is None:
+        path = [src]
+
+    if src == dst:
+        for p in path:
+            counts[p] += 1
+        return 1
+    if src == "out":
+        return 0
+
+    if src in counts:
+        return counts[src]
+
+    path.append(src)
+    s = sum(
+        dfs_count(graph, next_node, dst, path=path, counts=counts)
+        for next_node in graph[src]
+    )
+    path.pop()
+    counts[src] = s
+    return s
+
+
+def part_1(input: Input):
+    g = parse(input)
+    return bfs_count(g, "you", "out")
+
+
 def part_2(input: Input):
-    # for line in input:
-    #     print(line)
-    return 0
+    g = parse(input)
+
+    svr_to_dac = dfs_count(g, "svr", "dac")
+    svr_to_fft = dfs_count(g, "svr", "fft")
+    fft_to_dac = dfs_count(g, "fft", "dac")
+    dac_to_fft = dfs_count(g, "dac", "fft")
+    dac_to_out = dfs_count(g, "dac", "out")
+    fft_to_out = dfs_count(g, "fft", "out")
+
+    def prod(x: Iterable[int]) -> int:
+        return reduce(lambda a, b: a * b, x, 1)
+
+    return sum(
+        (
+            prod((svr_to_fft, fft_to_dac, dac_to_out)),
+            prod((svr_to_dac, dac_to_fft, fft_to_out)),
+        )
+    )
 
 
 def _test():
@@ -68,7 +138,7 @@ def _test():
         assert a == b, f"{a} != {b}"
 
     assert_eq(part_1(CONTROL_1), 5)
-    # assert_eq(part_2(CONTROL_1), 0)
+    assert_eq(part_2(CONTROL_2), 2)
 
 
 def _bench(fn, count=100):
