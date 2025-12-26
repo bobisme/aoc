4f01003a9a89310bf2a27e38033d71c1a5ca7347
Bob <bob@standardinput.co>
2025-12-26 11:45:37 -0500

    2025-09: new fmt

--- 2025/2025-09.py
+++ 2025/2025-09.py
@@ -4,8 +4,8 @@ from collections import deque
 from dataclasses import dataclass
 from functools import cache
 import itertools
+import time
 from typing import LiteralString, NamedTuple
-import timeit
 
 Input = list[str] | list[LiteralString]
 
@@ -21,9 +21,6 @@ CONTROL_1: Input = (
 7,3
 """.splitlines()
 )
-
-with open("2025-09.input") as f:
-    input_file = [line.rstrip("\n") for line in f.readlines()]
 
 Pos = NamedTuple("Pos", [("x", int), ("y", int)])
 
@@ -283,16 +280,16 @@ def _test():
     assert_eq(part_2(CONTROL_1), 24)
 
 
-def _bench(fn, count=100):
-    return timeit.timeit(fn, number=count) / count * 1_000
+def run(fn, year=2025, day=9, part=0):
+    start = time.perf_counter_ns()
+    res = fn()
+    elapsed_ns = time.perf_counter_ns() - start
+    print(f"{year}\t{day}\t{part}\t{res}\t{elapsed_ns}")
 
 
 if __name__ == "__main__":
+    with open("2025-09.input") as f:
+        input_file = [line.rstrip("\n") for line in f.readlines()]
     _test()
-    print("tests: PASS")
-    print("-" * 40)
-    print("part_1:", part_1(input_file))
-    print("part_2:", part_2(input_file))
-    print("-" * 40)
-    print("part_1 bench: {:.1f}ms".format(_bench(lambda: part_1(input_file), count=1)))
-    print("part_2 bench: {:.1f}ms".format(_bench(lambda: part_2(input_file), count=1)))
+    run(lambda: part_1(input_file), part=1)
+    run(lambda: part_2(input_file), part=2)
