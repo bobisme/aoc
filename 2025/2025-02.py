2a621f88202677738dc7f578fc2c794ad5993a14
Bob <bob@standardinput.co>
2025-12-28 00:14:35 -0500

    2025-02: fix printout

--- 2025/2025-02.py
+++ 2025/2025-02.py
@@ -283,7 +283,7 @@ def _test():
     assert_eq(part_2(CONTROL_1), 4174379265)
 
 
-def run(fn, year=2025, day=1, part=0):
+def run(fn, year=2025, day=2, part=0):
     start = time.perf_counter_ns()
     res = fn()
     elapsed_ns = time.perf_counter_ns() - start
