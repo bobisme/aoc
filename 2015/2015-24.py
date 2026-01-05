58710c7a5e62940d6bf4165dfad24c3aea9a4e5a
Bob <bob@standardinput.co>
2026-01-05 11:48:50 -0500

    2015-24: p1 *

---
+++ 2015/2015-24.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+from collections.abc import Iterator
+import itertools
+import math
+from typing import LiteralString
+import time
+
+Input = list[str] | list[LiteralString]
+
+CONTROL_1: Input = (
+    """\
+1
+2
+3
+4
+5
+7
+8
+9
+10
+11
+""".splitlines()
+)
+
+
+def get_weights(input: Input) -> tuple[int, ...]:
+    return tuple(map(int, input))
+
+
+def get_groups(weights: list[int]) -> Iterator[tuple[int, ...]]:
+    expected = sum(weights) // 3
+    smallest_group_size = 10000
+    for count in range(2, len(weights) // 2):
+        if count > smallest_group_size:
+            break
+        for comb in itertools.combinations(weights, count):
+            if sum(comb) == expected:
+                if count < smallest_group_size:
+                    smallest_group_size = count
+                yield comb
+
+
+def part_1(input: Input):
+    weights = list(get_weights(input))
+    weights.sort(reverse=True)
+    groups = get_groups(weights)
+    return math.prod(min(groups, key=lambda x: math.prod(x)))
+
+
+def part_2(input: Input):
+    weights = get_weights(input)
+    return 0
+
+
+def _test():
+    def assert_eq(a, b):
+        assert a == b, f"{a} != {b}"
+
+    assert_eq(part_1(CONTROL_1), 99)
+    # assert_eq(part_2(CONTROL_1), 0)
+
+
+def run(fn, year=2015, day=24, part=0):
+    start = time.perf_counter_ns()
+    res = fn()
+    elapsed_ns = time.perf_counter_ns() - start
+    print(f"{year}\t{day}\t{part}\t{res}\t{elapsed_ns}")
+    return res
+
+
+if __name__ == "__main__":
+    with open("2015-24.input") as f:
+        input_file = [line.rstrip("\n") for line in f.readlines()]
+    _test()
+    run(lambda: part_1(input_file), part=1)
+    # run(lambda: part_2(input_file), part=2)
